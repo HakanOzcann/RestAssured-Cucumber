581ebd1ba7dd52f4122bcb943cb9595fbd36a5be
HakanOzcann <hakan.ozcan44@hotmail.com>
2021-03-14 15:25:51 +0300

    RestAssured-Cucumber

---
+++ src/test/java/Runners/Runner.java
@@ -0,0 +1,18 @@
+package Runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+
+        features = {"src/test/java/Features"},
+        glue = {"StepDefinition"},
+        dryRun = false
+)
+
+public class Runner extends AbstractTestNGCucumberTests
+{
+
+
+
+}
